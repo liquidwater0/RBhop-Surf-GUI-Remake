acc936008b8c41aa6ba55f46e1f3bfab5e047f2f
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2021-12-30 18:53:55 -0600

    failed test

---
+++ js/menus/stylesMenu.js
@@ -0,0 +1,17 @@
+import { activateMenu } from "../menus.js";
+
+export function stylesMenu() {
+    const styleButtons = document.querySelectorAll("[data-style]");
+    const styleElement = document.querySelector("#style > p");
+
+    styleButtons.forEach(function(styleButton) { 
+        styleButton.addEventListener("click", function(event) {
+            changeStyle(styleButton.getAttribute("data-style"));
+            activateMenu(event.target.parentElement.parentElement.parentElement, false);
+        });
+    });
+
+    function changeStyle(style) {
+        styleElement.textContent = style;
+    }
+}
