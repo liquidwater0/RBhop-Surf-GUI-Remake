ed5b3b92c59f50867ce42cc72c4b310de3ae568b
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2021-12-31 14:08:18 -0600

    make code better

--- js/menus/stylesMenu.js
+++ js/menus/stylesMenu.js
@@ -4,14 +4,10 @@ export function stylesMenu() {
     const styleButtons = document.querySelectorAll("[data-style]");
     const styleElement = document.querySelector("#style > p");
 
-    styleButtons.forEach(function(styleButton) { 
-        styleButton.addEventListener("click", function(event) {
-            changeStyle(styleButton.getAttribute("data-style"));
-            activateMenu(event.target.parentElement.parentElement.parentElement.getAttribute("data-menu"), false);
-        });
-    });
+    styleButtons.forEach(function(styleButton) { styleButton.addEventListener("click", changeStyle) });
 
-    function changeStyle(style) {
-        styleElement.textContent = style;
+    function changeStyle(event) {
+        styleElement.textContent = event.target.getAttribute("data-style");
+        activateMenu(event.target.parentElement.parentElement.parentElement.getAttribute("data-menu"), false);
     }
 }
