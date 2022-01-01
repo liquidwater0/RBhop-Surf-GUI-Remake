c9209773df078194accdf6528191916778f23fd7
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-01-01 12:45:31 -0600

    speed

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ import { mainMenu } from "./menus/mainMenu.js";
 import { stylesMenu } from "./menus/stylesMenu.js";
 
 import { timer } from "./timer.js";
+import { speed } from "./speed.js";
 
 menus();
 mainMenu();
 stylesMenu();
 
 timer();
+speed();
