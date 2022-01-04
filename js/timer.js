d3b6277a13645dd4186a1344fd7036cccbf0c23e
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-01-04 15:08:50 -0600

    run completed functionality

--- js/timer.js
+++ js/timer.js
@@ -1,26 +1,27 @@
-export { timer, playPause, restart, paused }
+export { timer, restart, paused }
 
 document.addEventListener("keypress", function(event) { 
-    if (event.key == " ") playPause();
+    if (event.key == " ") switchPauseState();
     if (event.key == "r" || event.key == "R") restart();
+    if (event.key == "Enter") completeRun();
 });
 
 const timeElement = document.querySelector("#time > p:first-of-type");
 const personalBestElement = document.querySelector("#time > p:last-of-type");
 const timerProgressBar = document.getElementById("progressBar");
 
-const personalBest = {
-    minutes: "00",
-    seconds: "06",
-    milliseconds: "302"
+let paused = false;
+
+let personalBest = {
+    minutes: null,
+    seconds: null,
+    milliseconds: null
 }
 
-let paused = false;
+let personalBestMS = null;
 
 let sinceStarted = 0;
 let time = new Date();
-
-const personalBestMS = convertToMS(personalBest);
 
 function convertToMS(personalBest) { //function is not necessary but i want it for easy changing of the PB without needing to use a millisecond calculator
     const minutesMS = Number(personalBest.minutes * 1000 * 60);
@@ -48,7 +49,10 @@ function timer() {
         const converted = convertFromMS(sinceStarted);
         
         timeElement.textContent = `Time: ${converted.minutes}:${converted.seconds}.${converted.milliseconds}`;
-        personalBestElement.textContent = `Record: ${personalBest.minutes}:${personalBest.seconds}.${personalBest.milliseconds}`;
+        personalBestElement.textContent = 
+            personalBest.minutes == null || personalBest.seconds == null || personalBest.milliseconds == null ?
+            `Record: None` :
+            `Record: ${personalBest.minutes}:${personalBest.seconds}.${personalBest.milliseconds}`;
         
         const timePercentage = `${(sinceStarted / personalBestMS).toFixed(2) * 100}%`;
         
@@ -59,11 +63,33 @@ function timer() {
     }
 }
 
-function playPause() {
+function pause() {
+    paused = true;
+}
+
+function switchPauseState() {
     paused = !paused;
 }
 
 function restart() {
     sinceStarted = 0;
     timerProgressBar.style.width = "0%";
+}
+
+function completeRun() {
+    function setPersonalBest() {
+        const converted = convertFromMS(sinceStarted);
+
+        personalBest = {
+            minutes: converted.minutes,
+            seconds: converted.seconds,
+            milliseconds: converted.milliseconds
+        }
+
+        personalBestMS = convertToMS(personalBest);
+    }
+
+    if (personalBest.minutes == null || personalBest.seconds == null || personalBest.milliseconds == null || sinceStarted < personalBestMS) setPersonalBest();
+
+    pause();
 }
