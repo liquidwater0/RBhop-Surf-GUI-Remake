6e3bd1be3ed1407261f0bd9d0b1228491fbe32a5
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-01-05 16:49:06 -0600

    why does this work without being in main.js?? okay sure...

---
+++ js/chat.js
@@ -0,0 +1,35 @@
+export { chatBox };
+
+/*<li>
+    <span class="time">[5:00pm]</span>
+    <span class="bracket">[</span><span class="notice">Notice</span><span class="bracket">]</span>
+    Player 1 placed #10/1000000 in the style Autohop with a time of 42:64.057
+</li>*/
+
+const chatBox = document.getElementById("chatBox");
+
+chatBox.addEventListener("keydown", function(event) { if (event.key == "Enter") sendMessage() });
+
+function sendMessage() {
+    if (chatBox.value.trim() == "") return;
+
+    const date = new Date().toLocaleString("en-us", 
+        { 
+            hour: "numeric", 
+            minute: "2-digit", 
+            hour12: true 
+        });
+
+    const messageList = document.querySelector("#chat ul");
+    const playerName = "ong";
+
+    messageList.insertAdjacentHTML("beforeend", `
+        <li>
+            <span class="time">[${date}] 
+            </span><span class="player">${playerName}:</span> 
+            ${chatBox.value}
+        </li>
+    `);
+
+    chatBox.value = "";
+}
