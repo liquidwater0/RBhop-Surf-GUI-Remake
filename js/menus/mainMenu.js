8ef053ae7f79f83484054156e52f9dfd62cf6bea
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-01-06 15:58:04 -0600

    make it so if any text box is focused then don't do anything

--- js/menus/mainMenu.js
+++ js/menus/mainMenu.js
@@ -1,6 +1,5 @@
 import { activateMenu } from "../menus.js";
 import { restart } from "../timer.js";
-import { chatBox } from "../chat.js";
 
 export function mainMenu() {
     const restartButton = document.querySelector("[data-action='restart']");
@@ -9,7 +8,7 @@ export function mainMenu() {
     let isOpen = false;
 
     document.addEventListener("keypress", function(event) {
-        if (chatBox == document.activeElement) return;
+        if (document.activeElement.tagName == "INPUT") return;
 
         if (event.key == "m" || event.key == "M") activateMenu("mainMenu", isOpen);
         isOpen = !isOpen;
