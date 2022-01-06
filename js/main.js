f2ede8f179a6526fef35e5b3a476e8c9c423c8d2
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-01-06 15:56:16 -0600

    initialize chat and nameChange menu and rename playerlist function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 import { menus } from "./menus.js";
 import { mainMenu } from "./menus/mainMenu.js";
 import { stylesMenu } from "./menus/stylesMenu.js";
+import { nameChangeMenu } from "./menus/nameChangeMenu.js";
 
 import { timer } from "./timer.js";
 import { speed } from "./speed.js";
-import { playerList } from "./playerList.js";
+import { updatePlayerList } from "./playerList.js";
+import { chat } from "./chat.js";
 
 menus();
 mainMenu();
 stylesMenu();
+nameChangeMenu();
 
 timer();
 speed();
-playerList();
+updatePlayerList();
+chat();
