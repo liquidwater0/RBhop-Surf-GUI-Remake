8452e82ed1c1850c93aaf94da6767fc3e2e84999
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-01-06 22:40:05 -0600

    theme changer

--- js/menus/mainMenu.js
+++ js/menus/mainMenu.js
@@ -2,8 +2,13 @@ import { activateMenu } from "../menus.js";
 import { restart } from "../timer.js";
 
 export function mainMenu() {
+    document.addEventListener("DOMContentLoaded", setTheme);
+
     const restartButton = document.querySelector("[data-action='restart']");
+    const changeThemeButton = document.querySelector("[data-action='changeTheme']");
+
     restartButton.addEventListener("click", restart);
+    changeThemeButton.addEventListener("click", changeTheme);
 
     let isOpen = false;
 
@@ -13,4 +18,16 @@ export function mainMenu() {
         if (event.key == "m" || event.key == "M") activateMenu("mainMenu", isOpen);
         isOpen = !isOpen;
     });
+
+    let isDarkTheme = !localStorage.theme_RBS_GUI_Remake || localStorage.theme_RBS_GUI_Remake == "dark" ? true : false;
+
+    function setTheme() {
+        document.documentElement.setAttribute("data-theme", localStorage.theme_RBS_GUI_Remake || "dark");
+    }
+
+    function changeTheme() {
+        isDarkTheme = !isDarkTheme;
+        localStorage.theme_RBS_GUI_Remake = isDarkTheme ? "dark" : "light";
+        setTheme();
+    }
 }
