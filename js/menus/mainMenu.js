171c3ca8193519a0a295ec0ea033c22a1b32e9ed
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-01-07 18:53:15 -0600

    move theme changer out of mainMenu file

--- js/menus/mainMenu.js
+++ js/menus/mainMenu.js
@@ -2,13 +2,8 @@ import { activateMenu } from "../menus.js";
 import { restart } from "../timer.js";
 
 export function mainMenu() {
-    document.addEventListener("DOMContentLoaded", setTheme);
-
     const restartButton = document.querySelector("[data-action='restart']");
-    const changeThemeButton = document.querySelector("[data-action='changeTheme']");
-
     restartButton.addEventListener("click", restart);
-    changeThemeButton.addEventListener("click", changeTheme);
 
     let isOpen = false;
 
@@ -18,17 +13,4 @@ export function mainMenu() {
         if (event.key == "m" || event.key == "M") activateMenu("mainMenu", isOpen);
         isOpen = !isOpen;
     });
-
-    let isDarkTheme;
-
-    function setTheme() {
-        document.documentElement.setAttribute("data-theme", localStorage.theme_RBS_GUI_Remake || "dark");
-    }
-
-    function changeTheme() {
-        isDarkTheme = !localStorage.theme_RBS_GUI_Remake || localStorage.theme_RBS_GUI_Remake == "dark" ? true : false;
-        isDarkTheme = !isDarkTheme;
-        localStorage.theme_RBS_GUI_Remake = isDarkTheme ? "dark" : "light";
-        setTheme();
-    }
 }
