47a7915c483a7197adcac914bf5be73b9468b8c8
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-01-09 14:52:42 -0600

    Need to use ` key instead of tab cause browsers already use Tab for selecting elements

--- js/playerList.js
+++ js/playerList.js
@@ -4,10 +4,23 @@ function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+const playerListElement = document.getElementById("playerList");
 const yourPlayer = document.querySelector("#playerList [data-your-player]");
 const yourStyle = document.querySelector("#playerList [data-your-style]");
 const yourPersonalBest = document.querySelector("#playerList [data-your-personal-best]");
 const pingElements = document.querySelectorAll("[data-ping]");
+
+document.addEventListener("keydown", function(event) {
+    if (document.activeElement.tagName == "INPUT") return;
+
+    if (event.key == "`") playerListElement.classList.add("player-list-open");
+});
+
+document.addEventListener("keyup", function(event) {
+    if (document.activeElement.tagName == "INPUT") return;
+
+    if (event.key == "`") playerListElement.classList.remove("player-list-open");
+});
 
 function playerList() {
     yourPlayer.textContent = localStorage.playerName_RBS_GUI_Remake || "Player 1";
