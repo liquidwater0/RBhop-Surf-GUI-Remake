ecc4a75b6e5a5c41691bd1684087a5fe5e9ccd5b
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-01-09 00:34:49 -0600

    Ping, style, and personal best work

--- js/playerList.js
+++ js/playerList.js
@@ -1,6 +1,22 @@
-const playerListElement = document.querySelector("#playerList ul");
-const yourPlayer = playerListElement.children[1];
+export { playerList, yourStyle, yourPersonalBest };
 
-export function updatePlayerList() {
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const yourPlayer = document.querySelector("#playerList [data-your-player]");
+const yourStyle = document.querySelector("#playerList [data-your-style]");
+const yourPersonalBest = document.querySelector("#playerList [data-your-personal-best]");
+const pingElements = document.querySelectorAll("[data-ping]");
+
+function playerList() {
     yourPlayer.textContent = localStorage.playerName_RBS_GUI_Remake || "Player 1";
+    
+    setInterval(update, 200);
+
+    function update() {
+        pingElements.forEach(function(pingElement) {
+            pingElement.textContent = getRandomNumber(0, 1000);
+        });
+    }
 }
