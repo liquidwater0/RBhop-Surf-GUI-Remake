4c9d3bf49c95b7df39e22eb3ddd98b243407c79a
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-01-14 23:06:21 -0600

    moved menu imports to menus.js where it belongs

--- js/main.js
+++ js/main.js
@@ -1,18 +1,10 @@
 import { menus } from "./menus.js";
-import { mainMenu } from "./menus/mainMenu.js";
-import { stylesMenu } from "./menus/stylesMenu.js";
-import { settingsMenu } from "./menus/settingsMenu.js";
-
 import { timer } from "./timer.js";
 import { speed } from "./speed.js";
 import { playerList } from "./playerList.js";
 import { chat } from "./chat.js";
 
 menus();
-mainMenu();
-stylesMenu();
-settingsMenu();
-
 timer();
 speed();
 playerList();
