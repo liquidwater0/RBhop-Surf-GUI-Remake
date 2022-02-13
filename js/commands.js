c7c285e8b284ea1e4489a561978b316c81b6a358
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-02-12 22:59:49 -0600

    New command system
    Commands now have aliases! Commands can have more than 1 correct command. Commands can also have arguments but still need to figure out how to make optional arguments work.

--- js/commands.js
+++ js/commands.js
@@ -3,55 +3,111 @@ import { sendNoticeMessage } from "./chat.js";
 import { activateMenu } from "./menus.js";
 import { settings, updateAutoRestart, autoRestart } from "./menus/settingsMenu.js";
 
+const commands = [
+    {
+        name: "Restart",
+        description: "Restart the run.",
+        aliases: ["r", "restart"],
+        arguments: null,
+        activateCommand: () => restart()
+    }, {
+        name: "Styles",
+        description: "Open the styles menu.",
+        aliases: ["style", "styles"],
+        arguments: null,
+        activateCommand: () => activateMenu("styles", true)
+    }, {
+        name: "Settings",
+        description: "Open the settings menu.",
+        aliases: ["settings"],
+        arguments: null,
+        activateCommand: () => activateMenu("settings", true)
+    }, {
+        name: "Name Change",
+        description: "Open the name change menu.",
+        aliases: ["name"],
+        arguments: null,
+        activateCommand: () => activateMenu("nameChange", true)
+    }, {
+        name: "Theme",
+        description: "Change the theme.",
+        aliases: ["theme"],
+        arguments: ["dark", "light"],
+        activateCommand: argument => {
+            if (argument === "dark") {
+                document.documentElement.setAttribute("data-theme", "dark");
+                settings.theme = "dark";
+            } else if (argument === "light") {
+                document.documentElement.setAttribute("data-theme", "light");
+                settings.theme = "light";
+            }
+            
+            localStorage.RBS_GUI_Remake = JSON.stringify(settings);
+        }
+    }, {
+        name: "Controls",
+        description: "Open the controls menu.",
+        aliases: ["controls"],
+        arguments: null,
+        activateCommand: () => activateMenu("controls", true)
+    }, {
+        name: "Auto Restart",
+        description: "Open the styles menu.",
+        aliases: ["autorestart", "ar"],
+        arguments: ["on", "off"], //Make switched eventually
+        activateCommand: argument => {
+            if (argument === "on") {
+                settings.autoRestart = true;
+            } else if (argument === "off") {
+                settings.autoRestart = false;
+            }
+
+            localStorage.RBS_GUI_Remake = JSON.stringify(settings);
+        }
+    }
+]
+
 export function commandHandler(message) {
     const prefixes = ["!", "/"];
 
-    prefixes.forEach(function(prefix) {
-        if (message[0] == prefix) {
-            switch (message.slice(1)) {
-                case "r":
-                    restart();
-                    break;
-                case "styles":
-                    activateMenu("styles", true);
-                    break;
-                case "settings":
-                    activateMenu("settings", true);
-                    break;
-                case "name":
-                    activateMenu("nameChange", true);
-                    break;
-                case "theme dark":
-                    document.documentElement.setAttribute("data-theme", "dark");
-                    settings.theme = "dark";
-                    localStorage.RBS_GUI_Remake = JSON.stringify(settings);
-                    break;
-                case "theme light":
-                    document.documentElement.setAttribute("data-theme", "light");
-                    settings.theme = "light";
-                    localStorage.RBS_GUI_Remake = JSON.stringify(settings);
-                    break;
-                case "controls":
-                    activateMenu("controls", true);
-                    break;
-                case "autorestart":
-                    updateAutoRestart();
+    function checkPrefix(prefix) {
+        return message[0] == prefix;
+    }
 
-                    settings.autoRestart = autoRestart;
-                    localStorage.RBS_GUI_Remake = JSON.stringify(settings);
-                    break;
-                case "autorestart on":
-                    settings.autoRestart = true;
-                    localStorage.RBS_GUI_Remake = JSON.stringify(settings);
-                    break;
-                case "autorestart off":
-                    settings.autoRestart = false;
-                    localStorage.RBS_GUI_Remake = JSON.stringify(settings);
-                    break;
-                default:
-                    sendNoticeMessage(`The command "${message}" does not exist.`);
-                    break;
-            }
-        }
-    });
+    if (!prefixes.some(checkPrefix)) return;
+
+    const messageParts = message.slice(1).split(" ");
+
+    function checkCommand() {
+        let commandObject = {};
+
+        commands.forEach(command => {
+            command.aliases.forEach(alias => {
+                if (messageParts[0] === alias) commandObject.command = command;
+            });
+
+            if (command.arguments === null) return;
+
+            command.arguments.forEach(argument => {
+                if (messageParts[1] === argument) commandObject.correctArguments = true;
+            });
+        });
+
+        return commandObject;
+    }
+
+    const correctCommand = checkCommand().command;
+
+    if (!correctCommand) {
+        sendNoticeMessage(`The command "${message}" does not exist.`);
+        return;
+    }
+
+    //fix invalid arguments message for optional arguments
+    if (correctCommand.arguments !== null && !checkCommand().correctArguments) {
+        sendNoticeMessage(`Invalid arguments.`);
+        return;
+    }
+
+    correctCommand.activateCommand(messageParts[1]);
 }
