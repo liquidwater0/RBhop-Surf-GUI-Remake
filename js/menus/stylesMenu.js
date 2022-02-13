0c08442e53616d1e2fa8bb0eedd35b31e3cfcd3d
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-02-12 23:12:39 -0600

    arrow funcs

--- js/menus/stylesMenu.js
+++ js/menus/stylesMenu.js
@@ -1,4 +1,5 @@
-export { stylesMenu, styleElement }
+export { stylesMenu, styleElement };
+
 import { restart } from "../timer.js";
 import { yourStyle } from "../playerList.js";
 
@@ -6,7 +7,7 @@ const styleButtons = document.querySelectorAll("[data-style]");
 const styleElement = document.querySelector("#style > p");
 
 function stylesMenu() {
-    styleButtons.forEach(function(styleButton) { styleButton.addEventListener("click", changeStyle) });
+    styleButtons.forEach(styleButton => styleButton.addEventListener("click", changeStyle));
 
     function changeStyle(event) {
         styleElement.textContent = event.target.getAttribute("data-style");
