b699e6cf9b4734ef93ad6d1991afa1b92040376d
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-02-23 18:02:48 -0600

    showkeys

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ import { timer } from "./timer.js";
 import { speed } from "./speed.js";
 import { playerList } from "./playerList.js";
 import { chat } from "./chat.js";
+import { showkeys } from "./showkeys.js";
 
 menus();
 timer();
 speed();
 playerList();
 chat();
+showkeys();
