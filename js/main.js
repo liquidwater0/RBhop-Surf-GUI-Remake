6a87c15cef42e62267ca9b90b8da16e150393491
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-03-09 16:50:24 -0600

    don't need this here anymore

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 import { menus } from "./menus.js";
-import { timer } from "./timer.js";
 import { speed } from "./speed.js";
 import { playerList } from "./playerList.js";
 import { chat } from "./chat.js";
 import { showkeys } from "./showkeys.js";
 
 menus();
-timer();
 speed();
 playerList();
 chat();
