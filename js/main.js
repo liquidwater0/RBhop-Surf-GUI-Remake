9412e5fa301c0c8eaafd69dd5d5ce1934be188c4
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-03-12 17:46:18 -0600

    context menus

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ import { speed } from "./speed.js";
 import { playerList } from "./playerList.js";
 import { chat } from "./chat.js";
 import { showkeys } from "./showkeys.js";
+import { contextMenus } from "./contextMenus.js";
 
 menus();
 speed();
 playerList();
 chat();
 showkeys();
+contextMenus();
