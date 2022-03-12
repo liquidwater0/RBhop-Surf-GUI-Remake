8b2562117d7c310f92463d2242210fd171143af4
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-03-12 15:43:44 -0800

    context menus!

---
+++ js/contextMenus.js
@@ -0,0 +1,65 @@
+import { timer } from "./timer.js";
+import { activateMenu } from "./menus.js";
+
+class ContextMenu {
+    constructor(element, items) {
+        this.items = items;
+
+        element.addEventListener("contextmenu", event => {
+            if (event.ctrlKey || event.target !== element) return;
+
+            event.preventDefault();
+            this.open(event.pageX, event.pageY);
+        });
+
+        document.addEventListener("click", () => this.deleteContextMenus());
+    }
+
+    deleteContextMenus() {
+        const contextMenus = document.querySelectorAll("[data-context-menu]");
+        contextMenus.forEach(contextMenu => contextMenu.remove());
+    }
+
+    open(xPos, yPos) {
+        this.deleteContextMenus();
+
+        document.body.insertAdjacentHTML("afterbegin", `
+            <div class="transparent-blur" data-context-menu>
+                <ul></ul>
+            </div>
+        `);
+
+        const contextMenu = document.querySelector("[data-context-menu]");
+        const contextMenuList = document.querySelector("[data-context-menu] > ul");
+
+        this.items.forEach(item => {
+            const listItem = document.createElement("li");
+
+            listItem.textContent = item.label;
+            listItem.addEventListener("click", item.context);
+
+            contextMenuList.appendChild(listItem);
+        });
+
+        contextMenu.style.setProperty("--left", xPos);
+        contextMenu.style.setProperty("--top", yPos);
+    }
+}
+
+export function contextMenus() {
+    new ContextMenu(document.documentElement, [
+        {
+            label: "Restart Run",
+            context: () => timer.restart()
+        }, {
+            label: "Pause Run",
+            context: () => timer.control()
+        }, {
+            label: "Complete Run",
+            context: () => timer.completeRun()
+        }, {
+            label: "Open main menu",
+            context: () => activateMenu("mainMenu", true)
+        }
+    ]);
+}
