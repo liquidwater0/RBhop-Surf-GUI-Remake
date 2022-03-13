2d0a083666db4341b851dc17c90b27f9f0bb5e73
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-03-13 00:25:40 -0600

    rock the vote functionality

--- js/menus/mainMenu.js
+++ js/menus/mainMenu.js
@@ -1,7 +1,11 @@
+export { mainMenu, rockTheVote };
+
 import { activateMenu } from "../menus.js";
 import { timer } from "../timer.js";
+import { sendNoticeMessage } from "../chat.js";
+import { settings } from "./settingsMenu.js";
 
-export function mainMenu() {
+function mainMenu() {
     const restartButton = document.querySelector("[data-action='restart']");
     restartButton.addEventListener("click", () => timer.restart());
 
@@ -13,4 +17,20 @@ export function mainMenu() {
         if (event.key == "m" || event.key == "M") activateMenu("mainMenu", isOpen);
         isOpen = !isOpen;
     });
+
+    const rtvButton = document.querySelector("[data-action='rockTheVote']");
+    rtvButton.addEventListener("click", rockTheVote);
+}
+
+let hasRTVed = false;
+
+function rockTheVote() {
+    if (hasRTVed) {
+        sendNoticeMessage("You already voted!");
+        return;
+    }
+
+    sendNoticeMessage(`${settings.playerName || "Player 1"} wants to rock the vote. 10 more votes required to rock the vote.`);
+
+    hasRTVed = true;
 }
