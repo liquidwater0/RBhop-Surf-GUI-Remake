f8e3d8cc57375f7f7a8387d76502a6ab64865dee
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-03-14 15:53:55 -0500

    separate formatTime function and internal paused variable

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,4 @@
-export { timer, timerPaused };
+export { Timer, timer, timerPaused, formatTime };
 
 import { sendTimerMessage } from "./chat.js";
 import { styleElement } from "../js/menus/stylesMenu.js";
@@ -7,45 +7,40 @@ import { settings } from "./menus/settingsMenu.js";
 
 let timerPaused = false;
 
+function formatTime(ms) {
+    const minutes = String(parseInt((ms / 1000) / 60)).padStart(2, "0");
+    const seconds = String(parseInt((ms / 1000) % 60)).padStart(2, "0");
+    const milliseconds = String(parseInt(ms % 1000)).padStart(3, "0");
+
+    return `${minutes}:${seconds}.${milliseconds}`;
+}
+
 class Timer {
-    constructor(currentTimeElement, personalBestElement, progressBar) {
+    constructor(currentTimeElement, personalBestElement, progressBar, personalBest) {
         this.currentTimeElement = currentTimeElement;
         this.personalBestElement = personalBestElement;
         this.progressBar = progressBar;
 
+        this.paused = false;
+
         this.sinceStarted = 0;
         this.time = new Date();
-        this.personalBest = null;
+        this.personalBest = personalBest;
 
         this.timeConverted;
         this.personalBestConverted;
-
+        
         this.run();
     }
 
-    convertFromMS(number) {
-        return {
-            minutes: String(parseInt((number / 1000) / 60)).padStart(2, "0"),
-            seconds: String(parseInt((number / 1000) % 60)).padStart(2, "0"),
-            milliseconds: String(parseInt(number % 1000)).padStart(3, "0")
-        }
-    }
-
-    formatTime(minutes, seconds, milliseconds) {
-        return `${minutes}:${seconds}.${milliseconds}`;
-    }
-
     run() {
-        this.sinceStarted = timerPaused ? this.sinceStarted : this.sinceStarted += new Date() - this.time;
+        this.sinceStarted = this.paused ? this.sinceStarted : this.sinceStarted += new Date() - this.time;
         this.time = new Date();
 
         if (this.personalBest && this.sinceStarted > this.personalBest && settings.autoRestart == true) this.restart();
 
-        this.timeConverted = this.convertFromMS(this.sinceStarted);
-        this.personalBestConverted = this.convertFromMS(this.personalBest);
-
-        const currentTime = this.formatTime(this.timeConverted.minutes, this.timeConverted.seconds, this.timeConverted.milliseconds);
-        const personalBestTime = this.formatTime(this.personalBestConverted.minutes, this.personalBestConverted.seconds, this.personalBestConverted.milliseconds);
+        const currentTime = formatTime(this.sinceStarted);
+        const personalBestTime = formatTime(this.personalBest);
         
         this.currentTimeElement.textContent = `Time: ${currentTime}`;
         this.personalBestElement.textContent = 
@@ -53,7 +48,7 @@ class Timer {
             `Record: None` :
             `Record: ${personalBestTime}`;
         
-        const timePercentage = `${(this.sinceStarted / this.personalBest).toFixed(2) * 100}%`;
+        const timePercentage = this.personalBest ? `${(this.sinceStarted / this.personalBest).toFixed(2) * 100}%` : "0%";
         
         this.progressBar.style.width = timePercentage;
         this.progressBar.style.backgroundColor = (this.sinceStarted > this.personalBest) ? "red" : "rgb(0, 255, 0)";
@@ -63,7 +58,7 @@ class Timer {
 
     completeRun() {
         if (!this.personalBest || this.sinceStarted < this.personalBest) {
-            const currentTime = this.formatTime(this.timeConverted.minutes, this.timeConverted.seconds, this.timeConverted.milliseconds);
+            const currentTime = formatTime(this.sinceStarted);
 
             this.personalBest = this.sinceStarted;
     
@@ -78,11 +73,11 @@ class Timer {
             }
         }
     
-        timerPaused = true;
+        this.paused = true;
     }
 
     control() {
-        timerPaused = !timerPaused;
+        this.paused = !this.paused;
     }
 
     restart() {
@@ -100,7 +95,11 @@ const timer = new Timer(timeElement, personalBestElement, timerProgressBar);
 document.addEventListener("keydown", event => {
     if (document.activeElement.tagName === "INPUT") return;
 
-    if (event.shiftKey && event.key === " ") timer.control(); // idk why it won't work with Space + Shift????
+    if (event.shiftKey && event.key === " ") {
+        timer.control(); // idk why it won't work with Space + Shift????
+        timerPaused = !timerPaused;
+    }
+
     if (event.key.toLowerCase() === "r") timer.restart();
     if (event.key === "Enter") timer.completeRun();
 });
