37a554dc29da2d44b80b254a17188e2d56c216e8
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-03-14 12:59:57 -0500

    put context menus in respective file locations

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
-import { menus } from "./menus.js";
+import ContextMenu from "./contextMenus.js";
+import { menus, activateMenu } from "./menus.js";
 import { speed } from "./speed.js";
 import { playerList } from "./playerList.js";
 import { chat } from "./chat.js";
 import { showkeys } from "./showkeys.js";
-import { contextMenus } from "./contextMenus.js";
+import { timer } from "./timer.js";
 
 menus();
 speed();
 playerList();
 chat();
 showkeys();
-contextMenus();
+
+new ContextMenu(document.documentElement, [
+    {
+        label: "Restart Run",
+        action: () => timer.restart()
+    }, {
+        label: "Pause Run",
+        action: () => timer.control()
+    }, {
+        label: "Complete Run",
+        action: () => timer.completeRun()
+    }, {
+        label: "Open main menu",
+        action: () => activateMenu("mainMenu", true)
+    }
+]);
