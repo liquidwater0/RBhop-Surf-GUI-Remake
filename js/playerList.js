10cc1be0ec1f37b0d7a5f31593b5e2cfb3e044b7
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-03-14 22:00:09 -0500

    move spectate stuff into own file and clean up code here

--- js/playerList.js
+++ js/playerList.js
@@ -1,8 +1,9 @@
-export { playerList, yourStyle, yourPersonalBest };
+export { playerList, yourStyle, yourPersonalBest, playerListPlayers };
 
-import { settings } from "./menus/settingsMenu.js";
-import { Timer, formatTime } from "./timer.js";
 import ContextMenu from "./contextMenus.js";
+import { settings } from "./menus/settingsMenu.js";
+import { formatTime } from "./timer.js";
+import { spectate } from "./spectate.js";
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -40,17 +41,17 @@ function playerList() {
         const playerPersonalBest = Number(playerPersonalBestElement.getAttribute("data-personal-best"));
         const hasPersonalBest = playerPersonalBest <= 0 ? false : true;
 
+        const selectedPlayer = {
+            name: player.children[0].textContent,
+            style: player.children[2].textContent,
+            personalBest: playerPersonalBest,
+            isBot: player.classList.contains("bot")
+        };
+
         new ContextMenu(player, [
             {
                 label: "Spectate",
-                action: () => {
-                    spectate(
-                        player.classList.contains("bot"), //isBot
-                        player.children[0].textContent, //name
-                        player.children[2].textContent, //style
-                        playerPersonalBest
-                    );
-                }
+                action: () => spectate(selectedPlayer)
             }
         ]);
 
@@ -64,26 +65,4 @@ function playerList() {
     function update() {
         pingElements.forEach(pingElement => pingElement.textContent = getRandomNumber(0, 1000));
     }
-}
-
-function spectate(isBot, name, style, personalBest) {
-    const yourTimerElement = document.getElementById("timer");
-    const spectateTimerElement = document.getElementById("spectateTimer");
-    
-    const spectateTimeElement = document.querySelector("#spectateTimer #time p:nth-child(2)");
-    const spectatePersonalBestElement = document.querySelector("#spectateTimer p:nth-child(3)");
-    const spectateProgressBarElement = document.querySelector("#spectateTimer #time #progressBar");
-    const spectateStyleElement = document.querySelector("#spectateTimer #style p:first-of-type");
-    const spectateNameElement = document.querySelector("#spectateTimer #name p:first-of-type");
-
-    const botIcon = document.querySelector("#spectateTimer #name .bot");
-    botIcon.style.visibility = isBot ? "visible" : "hidden";
-
-    new Timer(spectateTimeElement, spectatePersonalBestElement, spectateProgressBarElement, personalBest);
-
-    yourTimerElement.style.display = "none";
-    spectateTimerElement.style.display = "block";
-
-    spectateStyleElement.textContent = style;
-    spectateNameElement.textContent = name;
 }
