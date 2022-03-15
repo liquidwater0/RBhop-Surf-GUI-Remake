f60f1bf3ee6a592d63a380a2fa3cb950aea3345d
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-03-14 22:20:19 -0500

    renamed variable

--- js/menus/stylesMenu.js
+++ js/menus/stylesMenu.js
@@ -1,7 +1,7 @@
 export { stylesMenu, styleElement };
 
 import { timer } from "../timer.js";
-import { yourStyle } from "../playerList.js";
+import { yourStyleTextElement } from "../playerList.js";
 
 const styleButtons = document.querySelectorAll("[data-style]");
 const styleElement = document.querySelector("#style > p");
@@ -11,7 +11,7 @@ function stylesMenu() {
 
     function changeStyle(event) {
         styleElement.textContent = event.target.getAttribute("data-style");
-        yourStyle.textContent = event.target.getAttribute("data-style");
+        yourStyleTextElement.textContent = event.target.getAttribute("data-style");
         timer.restart();
     }
 }
